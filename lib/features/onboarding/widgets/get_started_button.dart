02810f6db198a7303d7561e8d57c165ab37233b2
Elbarody <ahmedelbarody999@gmail.com>
2026-02-16 20:37:44 +0200

    -add images & svg for assets - added file doc app -add navigation extention -added routing folder -add colors and theme -login empty screen -onboarding screen

---
+++ lib/features/onboarding/widgets/get_started_button.dart
@@ -0,0 +1,25 @@
+import 'package:doctor_opportunity/core/theming/colors.dart';
+import 'package:doctor_opportunity/core/theming/styles.dart';
+import 'package:flutter/material.dart';
+
+class GetStartedButton extends StatelessWidget {
+  const GetStartedButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {
+        Navigator.pushNamed(context, '/loginScreen');
+      },
+      style: ButtonStyle(
+        backgroundColor: WidgetStateProperty.all(ColorsManager.mainBlue),
+        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+        minimumSize: WidgetStateProperty.all(const Size(double.infinity, 52)),
+        shape: WidgetStateProperty.all(
+          RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+        ),
+      ),
+      child: Text('Get Started', style: TextStyles.font16WhiteSemiBold),
+    );
+  }
+}
