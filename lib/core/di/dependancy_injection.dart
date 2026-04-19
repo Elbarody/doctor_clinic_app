84680a36e76d4fb11618cd72eec6efc305c98a7c
elbarody <ahmedelbarody999@gmail.com>
2026-04-19 16:01:29 +0200

    -added di -added app regex -integrated with login end point -added login repo and cubit -added login states with validation

---
+++ lib/core/di/dependancy_injection.dart
@@ -0,0 +1,18 @@
+import 'package:dio/dio.dart';
+import 'package:doctor_opportunity/core/networking/api_service.dart';
+import 'package:doctor_opportunity/core/networking/dio_factory.dart';
+import 'package:doctor_opportunity/features/login/data/repos/login_repo.dart';
+import 'package:doctor_opportunity/features/login/logic/cubit/login_cubit.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+
+Future<void> setupGetIt() async {
+  /// Dio & ApiService
+  Dio dio = DioFactory.getDio();
+  getIt.registerLazySingleton<ApiService>(() => ApiService(dio));
+
+  //login
+  getIt.registerLazySingleton<LoginRepo>(() => LoginRepo(getIt()));
+  getIt.registerLazySingleton<LoginCubit>(() => LoginCubit(getIt()));
+}
